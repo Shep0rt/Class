62fff49bc4ea52d927be8b66ae109ea96e568fbe
Pavel Michka <pavel.michka@gmail.com>
2022-11-21 11:56:58 +0600

    v1

--- Class/main.swift
+++ Class/main.swift
@@ -4,8 +4,34 @@
 //
 //  Created by Pavel Michka on 21.11.2022.
 //
-
 import Foundation
+//Классы
+//Задание 1
+//В чем принципиальное и самое важное отличие класса от структуры? Приведите пример, демонстрирующий ваш ответ.
+//Значение класса можно передать только по ссылке, а структуры только копированием
+class Class {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+var class1 = Class(name: "Pavel", age: 26)
+var class2 = class1
+class1.name
+class2.name
+class2.name = "Sergey"
+class1.name
 
-print("Hello, World!")
-
+struct Struct {
+    var name: String
+    var age : Int
+}
+var struct1 = Struct(name: "Pavel", age: 26)
+var struct2 = struct1
+struct1.name
+struct2.name
+struct2.name = "Sergey"
+struct1
